31394dbba7bd6f9dfba6cf2637f1d448041f7d97
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-02-26 17:51:40 +0400

    completed Slider Arcitect

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,26 @@ links.forEach(item=>{
     })
 })
 
+
+// ===============SWIPER-JS====================
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.prevBtn',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
