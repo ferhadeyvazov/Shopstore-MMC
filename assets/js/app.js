aaaa431779aa2a87599c7bd1b9a59dd4ca069f9e
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-03-07 15:13:12 +0400

    added topOfPage

--- assets/js/app.js
+++ assets/js/app.js
@@ -151,3 +151,15 @@ window.addEventListener('load', ()=>{
         item.disabled = false;
     })
 })
+
+// ===============TO TOP OF PAGE============================
+const toTop = document.querySelector('.btn-up');
+
+window.addEventListener('scroll', ()=>{
+    if(window.pageYOffset>100){
+        toTop.classList.add('to-active-btn');
+    }
+    else{
+        toTop.classList.remove('to-active-btn');
+    }
+})
