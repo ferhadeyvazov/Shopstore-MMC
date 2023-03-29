81f74b3eb355040b12ec01aedf253524d61900d0
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-03-29 17:23:45 +0400

    Mehsul-Catalogu bug fixed

--- assets/js/Modules/catalog/mehsulCatalogu.js
+++ assets/js/Modules/catalog/mehsulCatalogu.js
@@ -9,15 +9,15 @@ function catalogPopup() {
     if (popup.classList.contains("activePopUp")) {
         popupCatalog__container.classList.add('activeCatalog');
 
-        window.addEventListener('scroll', () => {
-            if (window.pageYOffset > 50) {
-                popupCatalog__container.classList.add('activeCatalog-90');
-            }
-            else {
-                popupCatalog__container.classList.remove('activeCatalog-90')
+        // window.addEventListener('scroll', () => {
+        //     if (window.pageYOffset > 50) {
+        //         popupCatalog__container.classList.add('activeCatalog-90');
+        //     }
+        //     else {
+        //         popupCatalog__container.classList.remove('activeCatalog-90')
                 
-            }
-        })
+        //     }
+        // })
         
     }
     else {
