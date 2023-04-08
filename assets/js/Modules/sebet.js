58fbcceed17767e2156ba29d63be06f64c1c9899
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-04-08 16:56:40 +0400

    Sebet hazirlanir ve Js yazilir

---
+++ assets/js/Modules/sebet.js
@@ -0,0 +1,12 @@
+let sebetProductsSayi = document.querySelectorAll(".marginContainer");
+
+// ===========Product Sayi ============================
+let sebetProductsButton = document.querySelector(".sebet__products--sayi");
+sebetProductsButton.innerText = sebetProductsSayi.length;
+
+console.log(sebetProductsButton);
+console.log(sebetProductsSayi.length);
+
+
+// ===========Product Silme ============================
+
