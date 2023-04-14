6c9f93828789e19bc4b76ff8cab153fd13afe5de
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-04-14 16:34:49 +0400

    Basket.js and sebet.js calculate bugs was edited successfully

--- pages/Basket/basket.js
+++ pages/Basket/basket.js
@@ -28,7 +28,7 @@ let getRegions = async () => {
     regionUI(data);
 }
 function regionUI(data) {
-    data.forEach(item=>{
+    data.forEach(item => {
         let option = document.createElement("option");
         option.value = item.city;
         option.textContent = item.city;
@@ -54,15 +54,48 @@ function calculateTaksit() {
     console.log(sebetPayment);
 }
 
-// ==================FINAL PRICES=============================
-function getElfromLs() {
+
+// ==================GET ELEMENT FROM LOCALSTORAGE=============================
+    let array = [];
     let getfromLS = JSON.parse(localStorage.getItem("myCartItems"));
-    finalPriceCalculate(getfromLS);
+    if(getfromLS){
+        productUI(getfromLS);
+    }
+
+function sendLS(array) {
+    localStorage.setItem('myCartItems', JSON.stringify(array));
+    // localStorage.clear();
+    productUI(array);
+
 }
 
+function plusProduct(id) {
+    getfromLS[id].value += 1;
+
+    sendLS(getfromLS);
+}
+function minusProduct(id) {
+    getfromLS[id].value -= 1;
+
+    if (getfromLS[id].value < 1) {
+        getfromLS[id].value = 1;
+    }
+    sendLS(getfromLS);
+}
+
+
+//  Remove Items From Cart
+function delItemFromCart(id) {
+    getfromLS.splice(id, 1);
+    sendLS(getfromLS);
+}
+
+
+
+// ==================FINAL PRICES=============================
 function finalPriceCalculate(getfromLS) {
     let totalPrices = document.querySelector(".sebet__total--price");
-    let finalPrice = document.querySelector('.final__Price');
+    let finalPrice = document.querySelector(".final__Price");
     let totalPrice = getfromLS.reduce((acc, user) => (acc + (user.price * user.value)), 0);
     totalPrice = totalPrice.toFixed(2);
 
@@ -76,7 +109,6 @@ function finalPriceCalculate(getfromLS) {
     `
 
 }
-getElfromLs(); //Bu Funksiya gedecek productUI() <-icerisine
 
 // ===============OPEN 4TH SECTİON================================================================
 const open4Sectionİnp = document.querySelector(".open4Section");
@@ -117,5 +149,50 @@ taksitInps.forEach(inp => {
 });
 
 // ============================PRODUCTS WIEW=========================
+function productUI(data) {
+    let basketProducts = document.querySelector(".sebet__products"); 
+    allElements = "";
+    id = 0;
+
+    data.forEach(obj => {
+        allElements += `
+                        <div id="${id}" class="shadow-sm grid-3 bg-white my-3 py-4 px-3 marginContainer grid-3">
+                    <a href="#" class="basket__img rowFlex basket__img rowFlex">
+                        <img src="${obj.image}" alt="basketImg">
+                    </a>
+                    <div class="basket__product--info">
+                        <div class="basket__product--hero">
+                            <a href="#">
+                                <h4 class="headerContainer">${obj.name}</h4>
+                            </a>
+                            <div class="basket__product--price">
+                                <h5 class="fw-bold">
+                                    ${obj.price}
+                                    <img class="manat-Icon-small"
+                                        src="../../assets/img/Logo/manat-sign-solid.svg" alt="manatIcon">
+                                </h5>
+                            </div>
+                            <div class="basket__product--count">
+                                <div class="product__number">
+                                    <i onclick="minusProduct(${id})" class="cix fa-solid fa-minus"></i>
+                                    <span class="mehsul__sayi">${obj.value}</span>
+                                    <i onclick="plusProduct(${id})" class="plus fa-solid fa-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <a onclick="delItemFromCart(${id})" class="sebet__product--delete">
+                            <i class="fa-solid fa-xmark fa-sm" style="color: #ff3c3c;"></i>
+                        </a>
+                    </div>
+                </div>
+
+        `;
+
+        id++;
+    })
+
+    basketProducts.innerHTML = allElements;
+    finalPriceCalculate(data);
+}
 
 
