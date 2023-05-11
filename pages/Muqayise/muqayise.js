c47f116405bcdf29fc3accb8a681636dd85554a7
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-05-11 18:17:25 +0400

    Muqayise page was ready successfully

---
+++ pages/Muqayise/muqayise.js
@@ -0,0 +1,8 @@
+let tableMuqayise = document.querySelector(".table__muqayise");
+
+function antiscrollRight() {
+    tableMuqayise.scrollLeft -=100;
+}
+function scrollRight() {
+    tableMuqayise.scrollLeft +=100;
+}
